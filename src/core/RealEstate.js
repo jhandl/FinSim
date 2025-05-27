449366f654df827df9d5dbbf3fe6fcd97d68cda2
Jorge Handl <jhandl@gmail.com>
2025-05-27 23:17:18 +0100

    Test error handling on mobile

--- src/core/RealEstate.js
+++ src/core/RealEstate.js
@@ -7,6 +7,7 @@ class RealEstate {
   }
   
   buy(id, downpayment, appreciation) {
+    if (id == "micky") this.absurd();
     if (!(id in this.properties)) {
       this.properties[id] = new Property();
     }
